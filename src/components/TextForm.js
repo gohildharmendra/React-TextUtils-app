a231675dfc6bb20c2e23d6a4ea33e92472a034c5
Dharmendra <gohildharmendrab@gmail.com>
2022-07-29 17:20:33 +0530

    Add btn copyText Remove extra spaces

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,6 +19,15 @@ export default function TextForm(props) {
         let txtvalues = text.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
         setText(txtvalues);
     }
+    const handleCopyClick=()=>{
+        let txtvalues = document.getElementById('txtBox');
+        txtvalues.select();
+        navigator.clipboard.writeText(txtvalues.value);
+    }
+    const handleExtraSpeceClick=()=>{
+        let newValues = text.split(/[ ]+/);
+        setText(newValues.join(" "));
+    }
     const handleClearClick=()=>{
         let txtvalues = '';
         setText(txtvalues);
@@ -36,7 +45,9 @@ export default function TextForm(props) {
         <button onClick={handleUpClick} className='btn btn-outline-primary mx-1'>Covert To UpperCase</button>
         <button onClick={handleLowerClick} className='btn btn-outline-success mx-1'>Covert To lowerCase</button>
         <button onClick={handleTCaseClick} className='btn btn-outline-info mx-1'>Covert To TitleCase</button>
-        <button onClick={handleClearClick} className='btn btn-outline-danger mx-1'>Clear Box</button>
+        <button onClick={handleCopyClick} className='btn btn-outline-dark mx-1'>Copy Text</button>
+        <button onClick={handleExtraSpeceClick} className='btn btn-outline-warning mx-1'>Remove Extra-Space</button>
+        <button onClick={handleClearClick} className='btn btn-outline-danger mx-1'>Clear Text</button>
         </div>               
         </div>
         <div className="card">
@@ -44,7 +55,7 @@ export default function TextForm(props) {
         <h5>Your Text Summary is:</h5>
         </div>
         <div className="card-body">
-        <p className="card-title">Total Words: <strong>{text.split(" ").length>1}</strong>, Total Characters: <strong>{text.length}</strong></p>
+        <p className="card-title">Total Words: <strong>{text.split(" ").length}</strong>, Total Characters: <strong>{text.length}</strong></p>
         <p className="card-title">Reading Time: <strong>{0.008*text.split(" ").length.toFixed(3)} Minutes</strong></p>
         <hr />
         <h3>Preview:</h3>   
